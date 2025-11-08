e749f9cb5a6417cd8bb24bbce896187566a04d0a
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-11-08 23:36:06 +0100

    update

--- Client/src/components/LatestJobCards.jsx
+++ Client/src/components/LatestJobCards.jsx
@@ -26,7 +26,7 @@ function LatestJobCards({ job }) {
           {job?.jobType}
         </Badge>
         <Badge className="text-[#7209b7] font-bold" variant="ghost">
-          {job?.salary} LPA
+          $ {job?.salary}
         </Badge>
 
       </div>
